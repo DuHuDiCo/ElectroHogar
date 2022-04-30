bc9a8619c0e995b821ad669ad1b07269541caa1d
Duvan Diaz <dudicodiaz@gmail.com>
2022-04-29 23:39:20 -0500

    commint 29/04/2022 23:38
    se agrega el backen del modulo de bancos donde se realiza el guardado de tipos de pagos y de guardado de banco de acuardo al tipo de pago ingresado

--- src/main/java/Datos/DaoAdmin.java
+++ src/main/java/Datos/DaoAdmin.java
@@ -2,6 +2,8 @@
 package Datos;
 
 import Dominio.Estados;
+import Dominio.Plataforma;
+import Dominio.TipoPago;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -14,6 +16,11 @@ public class DaoAdmin {
     
     private static final String SQL_INSERT_ESTADOS = "INSERT INTO estado(nombre_estado) VALUES (?)";
     private static final String SQL_SELEC_ESTADOS =  "SELECT * FROM estado";
+    private static final String SQL_INSERT_TIPOPAGO = "INSERT INTO tipoPago(tipo_pago) VALUES (?)";
+    private static final String SQL_SELEC_TIPOPAGO =  "SELECT * FROM tipoPago";
+    private static final String SQL_INSERT_BANCO = "INSERT INTO plataforma(nombre_plataforma, id_tipoPago) VALUES (?, ?)";
+    private static final String SQL_SELEC_BANCO =  "SELECT plataforma.idPlataforma, plataforma.nombre_plataforma,  tipoPago.idTipoPago, tipoPago.tipo_pago "
+            + " FROM plataforma INNER JOIN tipoPago ON plataforma.id_tipoPago = tipoPago.idTipoPago";
     
     
     
@@ -75,6 +82,128 @@ public class DaoAdmin {
         }
 
         return stated;
+    }
+    
+    
+    public int crearTipoPago(String tipo_pago) throws ClassNotFoundException{
+        Connection con = null;
+        PreparedStatement stmt = null;
+
+        int rown = 0;
+        try {
+            con = Conexion.getConnection();
+            stmt = con.prepareStatement(SQL_INSERT_TIPOPAGO);
+            stmt.setString(1, tipo_pago);
+            
+            rown = stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(con);
+            Conexion.close(stmt);
+
+        }
+        return rown;
+    }
+    
+    
+    public List<TipoPago> listarTipoPago() throws ClassNotFoundException {
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        TipoPago pago = null;
+
+        List<TipoPago> stated = new ArrayList<>();
+
+        try {
+            con = Conexion.getConnection();
+            stmt = con.prepareStatement(SQL_SELEC_TIPOPAGO);
+            
+
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                int idTipoPago = rs.getInt("idTipoPago");
+                String tipo_pago = rs.getString("tipo_pago");
+                
+
+                pago = new TipoPago(idTipoPago, tipo_pago);
+                stated.add(pago);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(con);
+            Conexion.close(stmt);
+            Conexion.close(rs);
+        }
+
+        return stated;
+    }
+    
+    
+    public int crearBanco (String banco, int tipo_pago) throws ClassNotFoundException{
+        Connection con = null;
+        PreparedStatement stmt = null;
+
+        int rown = 0;
+        try {
+            con = Conexion.getConnection();
+            stmt = con.prepareStatement(SQL_INSERT_BANCO);
+            stmt.setString(1, banco);
+            stmt.setInt(2, tipo_pago);
+            
+            rown = stmt.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(con);
+            Conexion.close(stmt);
+
+        }
+        return rown;
+    }
+    
+    
+    
+     public List<Plataforma> listarBancos() throws ClassNotFoundException {
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Plataforma banco = null;
+
+        List<Plataforma> bancos = new ArrayList<>();
+
+        try {
+            con = Conexion.getConnection();
+            stmt = con.prepareStatement(SQL_SELEC_BANCO);
+            
+
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                int idBanco = rs.getInt("idPlataforma");
+                String nombre_plataforma = rs.getString("nombre_plataforma");
+                int idTipoPago = rs.getInt("idTipoPago");
+                String tipo_pago = rs.getString("tipo_pago");
+                
+
+                banco = new Plataforma(idBanco, nombre_plataforma, idTipoPago, tipo_pago);
+                bancos.add(banco);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(con);
+            Conexion.close(stmt);
+            Conexion.close(rs);
+        }
+
+        return bancos;
     }
     
 }
--- src/main/java/Web/ServleControladorAdministrador.java
+++ src/main/java/Web/ServleControladorAdministrador.java
@@ -2,6 +2,8 @@ package Web;
 
 import Datos.DaoAdmin;
 import Dominio.Estados;
+import Dominio.Plataforma;
+import Dominio.TipoPago;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -30,6 +32,26 @@ public class ServleControladorAdministrador extends HttpServlet {
                     }
                 }
                 break;
+                case "llenarTipoPago":
+                {
+                    try {
+                        this.llenarTipoPago(req, resp);
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(ServleControladorAdministrador.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                    break;
+                case "listarBancos":
+                {
+                    try {
+                        this.listarBancos(req, resp);
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(ServleControladorAdministrador.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                    break;
+
+
 
             }
         }
@@ -48,6 +70,26 @@ public class ServleControladorAdministrador extends HttpServlet {
                     }
                 }
                 break;
+                case "crearTipoPago":
+                {
+                    try {
+                        this.crearTipoPago(req,resp);
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(ServleControladorAdministrador.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                    break;
+                case "crearBanco":
+                {
+                    try {
+                        this.crearBanco(req, resp);
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(ServleControladorAdministrador.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                    break;
+
+
 
             }
         }
@@ -72,6 +114,64 @@ public class ServleControladorAdministrador extends HttpServlet {
         Gson gson = new Gson();
 
         String json = gson.toJson(estados);
+        resp.setContentType("application/json");
+
+        PrintWriter out = resp.getWriter();
+
+        out.print(json);
+        out.flush();
+    }
+
+    private void crearTipoPago(HttpServletRequest req, HttpServletResponse resp) throws ClassNotFoundException, IOException {
+        String tipo_pago = req.getParameter("tipo_pago");
+        
+        int valid = new DaoAdmin().crearTipoPago(tipo_pago);
+        
+        String respuesta = Integer.toString(valid);
+        resp.setContentType("text/plain");
+
+        PrintWriter out = resp.getWriter();
+
+        out.print(respuesta);
+        out.flush();
+        
+    }
+
+    private void llenarTipoPago(HttpServletRequest req, HttpServletResponse resp) throws ClassNotFoundException, IOException {
+        List<TipoPago> tipoPago = new DaoAdmin().listarTipoPago();
+
+        Gson gson = new Gson();
+
+        String json = gson.toJson(tipoPago);
+        resp.setContentType("application/json");
+
+        PrintWriter out = resp.getWriter();
+
+        out.print(json);
+        out.flush();
+    }
+
+    private void crearBanco(HttpServletRequest req, HttpServletResponse resp) throws ClassNotFoundException, IOException {
+        String banco = req.getParameter("banco");
+        int tipo_pago = Integer.parseInt(req.getParameter("tipo_pago"));
+        
+        int respu = new DaoAdmin().crearBanco(banco, tipo_pago);
+        
+        String respuesta = Integer.toString(respu);
+        resp.setContentType("text/plain");
+
+        PrintWriter out = resp.getWriter();
+
+        out.print(respuesta);
+        out.flush();
+    }
+
+    private void listarBancos(HttpServletRequest req, HttpServletResponse resp) throws ClassNotFoundException, IOException {
+        List<Plataforma> bancos = new DaoAdmin().listarBancos();
+
+        Gson gson = new Gson();
+
+        String json = gson.toJson(bancos);
         resp.setContentType("application/json");
 
         PrintWriter out = resp.getWriter();
