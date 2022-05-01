f6aeca9e2062eea43c7cf9cd8c390a6ef837f98c
Duvan Diaz <dudicodiaz@gmail.com>
2022-04-30 21:50:28 -0500

    commit 30/04/2022
    en proceso de trabajo del backen en el guardar consignacion

--- src/main/java/Web/ServletControladorCartera.java
+++ src/main/java/Web/ServletControladorCartera.java
@@ -1,38 +1,59 @@
 package Web;
 
-
+import Datos.DaoCartera;
+import Dominio.Actualizacion;
+import Dominio.Consignacion;
+import Dominio.Archivo;
+import Dominio.Plataforma;
+import com.google.gson.Gson;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Date;
+import java.sql.SQLException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
 @MultipartConfig
 @WebServlet(urlPatterns = {"/ServletControladorCartera"})
 public class ServletControladorCartera extends HttpServlet {
+
     private final String rutaFiles = "C:\\Users\\DUVAN\\Documents\\GitHub\\ElectroHogar\\src\\main\\webapp\\files\\";
-    private File uploads = new File(rutaFiles);
-    private String[] extens = {".ico", ".png", ".jpg", ".jpeg"};
-    
+    private final File uploads = new File(rutaFiles);
+    private final String[] extens = {".ico", ".png", ".jpg", ".jpeg"};
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         String accion = req.getParameter("accion");
         if (accion != null) {
             switch (accion) {
+                case "llenarBanco":
+                {
+                    try {
+                        this.llenarBanco(req, resp);
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(ServletControladorCartera.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                    break;
+
             }
         }
     }
@@ -42,63 +63,102 @@ public class ServletControladorCartera extends HttpServlet {
         String accion = req.getParameter("accion");
         if (accion != null) {
             switch (accion) {
-                case "guardarConsignacion":
-                    this.guardarConsignacion(req, resp);
-                    break;
+                case "guardarConsignacion": {
+                    try {
+                        this.guardarConsignacion(req, resp);
+                    } catch (ClassNotFoundException | SQLException ex) {
+                        Logger.getLogger(ServletControladorCartera.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                break;
+
             }
         }
     }
 
-    private void guardarConsignacion(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+    private void guardarConsignacion(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException, ClassNotFoundException, SQLException {
         String num_recibo = req.getParameter("num_recibo");
         float valor = Float.valueOf(req.getParameter("valor"));
         String fecha = req.getParameter("fecha_pago");
         Date fecha_pago = fechaSQL(fecha);
-        Date fecha_consignacion = obtenerFechaServer();
-        String plataforma = req.getParameter("plataforma");
-        String tipo_pago = req.getParameter("tip_pago");
+        Date fecha_creacion = obtenerFechaServer();
+        int plataforma = Integer.parseInt(req.getParameter("plataforma"));
+       
         Part part = req.getPart("file");
-        
-        
-        if(part == null){
+        int id_estado = obtenerIdEstado("Pendiente");
+        //obtenemos el ID del usuario
+        HttpSession session = req.getSession(true);
+        String emaiUser = (String) session.getAttribute("usuario");
+        int id_usuario = new DaoCartera().obtenerIdUsuario(emaiUser);
+
+        if (part == null) {
             System.out.println("no ha seleccionado un archivo");
             return;
         }
-        
-        if(isExtension(part.getSubmittedFileName(), extens)){
+
+        if (isExtension(part.getSubmittedFileName(), extens)) {
+
+            String name = "Con" + num_recibo + "/" + fecha_creacion + "";
             String photo = saveFile(part, uploads);
+
+            Archivo file = new Archivo(name, photo);
+            // guardamos el archivo en la BD
+            int save = new DaoCartera().guardarArchivo(file);
+            //obtenemos el ID de ese archivo
+            int idFile = new DaoCartera().obtenerIdFile(name);
+            //Guardamos la primera actualizacion(por defecto:pendiente)
+            Actualizacion actu = new Actualizacion(fecha_creacion, id_estado, id_usuario);
+            int upd = new DaoCartera().guardarActualizacion(actu);
+            int idActu = new DaoCartera().obtenerIdActualizacion();
+
+            //Guardamos la consignacion en la BD
+            Consignacion consig = new Consignacion(num_recibo, fecha_creacion, fecha_pago, valor, idFile, id_usuario, id_estado, id_usuario, plataforma);
+            int SaveConsig = new DaoCartera().guardarConsignacion(consig);
+
+            String respuesta = Integer.toString(SaveConsig);
+            resp.setContentType("text/plain");
+
+            PrintWriter out = resp.getWriter();
+
+            out.print(respuesta);
+            out.flush();
+
         }
-        
 
     }
-    
-    
-    private String saveFile(Part part, File pathUploads){
+
+    private int obtenerIdEstado(String dato) throws ClassNotFoundException, SQLException {
+
+        int estado = new DaoCartera().obtenerIdEstado(dato);
+        return estado;
+
+    }
+
+    private String saveFile(Part part, File pathUploads) {
         String rutaAbsoluta = "";
-        
+
         try {
-            
+
             Path path = Paths.get(part.getSubmittedFileName());
             String fileName = path.getFileName().toString();
             InputStream input = part.getInputStream();
-            
-            if(input != null){
+
+            if (input != null) {
                 File file = new File(pathUploads, fileName);
                 rutaAbsoluta = file.getAbsolutePath();
                 Files.copy(input, file.toPath());
             }
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
         return rutaAbsoluta;
     }
-    
-    
-    private boolean isExtension(String fileName, String[] extensions){
-        for(String et:extensions){
-            if(fileName.toLowerCase().endsWith(et)){
+
+    private boolean isExtension(String fileName, String[] extensions) {
+        for (String et : extensions) {
+            if (fileName.toLowerCase().endsWith(et)) {
                 return true;
             }
         }
@@ -113,19 +173,33 @@ public class ServletControladorCartera extends HttpServlet {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        
+
         java.sql.Date sqlDate = new java.sql.Date(javaDate.getTime());
         return sqlDate;
     }
-    
-    private Date obtenerFechaServer(){
+
+    private Date obtenerFechaServer() {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Calendar calendar = Calendar.getInstance();
-        
+
         java.util.Date datObj = calendar.getTime();
         String formattedDate = sdf.format(datObj);
         java.sql.Date dateformated = fechaSQL(formattedDate);
-        
+
         return dateformated;
+    }
+
+    private void llenarBanco(HttpServletRequest req, HttpServletResponse resp) throws ClassNotFoundException, IOException {
+        List<Plataforma> banco = new DaoCartera().listarBanco();
+
+        Gson gson = new Gson();
+
+        String json = gson.toJson(banco);
+        resp.setContentType("application/json");
+
+        PrintWriter out = resp.getWriter();
+
+        out.print(json);
+        out.flush();
     }
 }
--- target/ElectroHogar-1.0/javascript/CarteraJS/consignaciones.js
+++ target/ElectroHogar-1.0/javascript/CarteraJS/consignaciones.js
@@ -1,7 +1,9 @@
 
-$(function() {
-    $('.input-file__input').on('change', function() {
-        if($(this)[0].files[0]) {
+/* global Swal */
+
+$(function () {
+    $('.input-file__input').on('change', function () {
+        if ($(this)[0].files[0]) {
             $(this).prev().text($(this)[0].files[0].name);
         }
     });
@@ -9,29 +11,124 @@ $(function() {
 
 
 
+function guardarConsignacion() {
+    alert("hola");
 
-var form = document.getElementById('form');
-
-form.onsubmit = e =>{
-    var fd = new FormData(form);
-    alert(fd);
-    e.preventDefault();
+    var datos = {};
     
+    datos.num_recibo = document.getElementById('txtNumRecibo').value;
+    datos.valor = document.getElementById('txtValor').value;
+    datos.fecha_pago = document.getElementById('dateCreacion').value;
+    datos.plataforma = document.getElementById('sltBancoCartera').value;
+    datos.file = document.getElementById('file').files;
     
-     $.ajax({
+    $.ajax({
         method: "POST",
-        url: "ServletControlador?accion=guardarConsignacion",
-        data: fd
+        url: "ServletControladorCartera?accion=guardarConsignacion",
+        data: datos,
+        contentType: 'Json'
         
     }).done(function (data) {
-        
-    };
 
-        
-    
-    
-};
+        var datos = data;
+        alert(datos);
 
-    
+
+        if (datos !== null) {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Inicio Exitoso',
+                showConfirmButton: false,
+                timer: 6500
+
+            });
+            alert(datos.nombre_rol);
+            roles(datos.nombre_rol);
+            
+
+
+        }
+
+  
+        // imprimimos la respuesta
+    }).fail(function () {
+
+        window.location.replace("login.html");
+    }).always(function () {
+
+    });
+
+
+
+}
+
+//function cargarDatosPago() {
+//
+//    event.preventDefault();
+//
+//    $.ajax({
+//        method: "GET",
+//        url: "ServletControladorAdministrador?accion=llenarTipoPago"
+//
+//    }).done(function (data) {
+//        var datos = JSON.stringify(data);
+//        var json = JSON.parse(datos);
+//        var html = "";
+//
+//        $.each(json, function (key, value) {
+//            $("#sltTipoPagoCartera").append('<option value="' + value.idTipoPago + '" >' + value.tipo_pago + '</option>');
+//        });
+//        
+//
+//
+//
+//    }).fail(function () {
+//
+//        window.location.replace("login.html");
+//    }).always(function () {
+//
+//    });
+//
+//
+//}
+
+
+function cargarDatosBanco() {
+
+    event.preventDefault();
+
+    $.ajax({
+        method: "GET",
+        url: "ServletControladorCartera?accion=llenarBanco"
+
+    }).done(function (data) {
+        var datos = JSON.stringify(data);
+        var json = JSON.parse(datos);
+        var html = "";
+
+        $.each(json, function (key, value) {
+            $("#sltBancoCartera").append('<option value="' + value.idPlataforma + '" >' + value.nombre_plataforma+'--'+value.tipo_pago + '</option>');
+        });
+        
+
+
+
+    }).fail(function () {
+
+        window.location.replace("login.html");
+    }).always(function () {
+
+    });
+
+
+}
+
+
+
+
+
+
+
 
 
